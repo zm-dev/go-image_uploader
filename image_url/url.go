7de28ba90e6b4a6bac197ebbc879c985a0fb9284
taoyu <1255803549@qq.com>
2018-05-21 17:08:38 +0800

    update default options

--- image_url/url.go
+++ image_url/url.go
@@ -9,7 +9,7 @@ var (
 	XSOptions = []Option{Width(50), Height(50), Quality(90)}
 	SMOptions = []Option{Width(100), Height(100), Quality(90)}
 	MDOptions = []Option{Width(200), Height(200), Quality(90)}
-	LGOptions = []Option{Width(400), Height(400), Quality(90)}
+	LGOptions = []Option{Width(500), Quality(90)}
 )
 
 type options struct {
